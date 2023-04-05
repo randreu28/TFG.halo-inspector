f2b4dd993b404f6ed153eeef70269f72950761c4
Rubén Chiquin <randreu28@gmail.com>
2023-04-05 17:07:17 +0200

    Displays the model with it's idle animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import { Model } from "./Model";
+import { Environment, OrbitControls } from "@react-three/drei";
+
 export default function App() {
   return (
-    <div className="flex h-screen justify-center items-center">Hello world</div>
+    <div className="h-screen w-screen">
+      <Canvas>
+        <pointLight position={[2, 2, 2]} />
+        <pointLight position={[-2, -2, -2]} />
+        <Environment preset="night" />
+        <OrbitControls maxDistance={3} minDistance={1} />
+        <Model position={[0, -1, 0]} />
+      </Canvas>
+    </div>
   );
 }
