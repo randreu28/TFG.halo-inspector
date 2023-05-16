943a6b4a0c3b1dadc65b96c02f377882e47c9e73
Rubén Chiquin <randreu28@gmail.com>
2023-05-16 17:49:45 +0200

    Adds a readme
    And updates the signature

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -1,23 +1,21 @@
 export default function Signature() {
   return (
     <>
-      <span className="absolute top-10 right-10 z-10 text-right">
+      <a
+        target="_blank"
+        href="https://tfg-docs.vercel.app/"
+        className="absolute top-10 right-10 z-10 text-right hover:underline"
+      >
         <p>Bachelor's Thesis</p>
-        <a
-          href="https://tfg-docs.vercel.app/"
-          className="hover:underline"
-          target="_blank"
-        >
-          Interactive 3D components
-        </a>
-      </span>
+        <p>Interactive 3D components</p>
+      </a>
 
-      <p className="absolute right-10 bottom-10 z-10 space-y-5">11/04/2023</p>
+      <p className="absolute right-10 bottom-10 z-10 space-y-5">16/06/2023</p>
 
       <a
         href="https://randreu.dev"
         target="_blank"
-        className="absolute bottom-10 left-10 z-10 max-w-xl flex gap-5"
+        className="absolute bottom-10 left-10 z-10 max-w-xl flex gap-5 hover:underline"
       >
         <svg
           className="text-white fill-current h-10 w-fit"
